4a2c3cd403c256704dd15eee68c5225ec2dc260d
Juntong20XX <Juntong20XX@gmail.com>
2026-02-23 22:35:41 -0500

    fix: model `kernel` moved

--- app/kernel/__init__.py
+++ app/kernel/__init__.py
@@ -3,10 +3,10 @@
 """
 
 from .connector import (Config, clean, setup, start_qemu, stop_qemu, build, debug, ASMLine, ASMParam, ASMStep,
-                        ASMLineReader, ALoader)
+                        ASMLineReader, ALoader, qemu_processes)
 from .animation import adl_models
 
 __all__ = ["Config", "clean", "setup", "start_qemu", "stop_qemu", "build", "debug",
            "ASMLine", "ASMParam", "ASMStep", "ASMLineReader",
-           "ALoader",
+           "ALoader", "qemu_processes",
            "adl_models"]
